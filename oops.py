72ebc7279d03cff4c9ab604836d871a25e83db31
Akrathod007 <anshrathod2104@gmail.com>
2026-07-13 10:34:24 +0530

    Added

--- oops.py
+++ oops.py
@@ -129,47 +129,147 @@
 # p1.welcome()
 
 
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+
+# p1 = Person("Raju", 30)
+
+# del p1.age
+
+# print(p1.name)
+# # print(p1.age) #Error
+
+
+# class Person:
+#     species = "Human"  # Class property
+
+#     def __init__(self, name):
+#         self.name = name  # Instance property
+
+
+# p1 = Person("Hatori")
+# p2 = Person("Doremee")
+
+# print(p1.name)
+# print(p2.name)
+# print(p1.species)
+# print(p2.species)
+
+
+# class Person:
+#     lastname = ""
+
+#     def __init__(self, name):
+#         self.name = name
+
+
+# p1 = Person("Ram")
+# p2 = Person("Shyam")
+
+# Person.lastname = "Pandey"
+
+# print(p1.lastname)
+# print(p2.lastname)
+
+
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def multiply(self, a, b):
+        return a * b
+
+
+calc = Calculator()
+print(calc.add(5, 3))
+print(calc.multiply(4, 7))
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def celebrate_birthday(self):
+        self.age += 1
+        print(f"Happy birthday! You are now {self.age}")
+
+
+p1 = Person("Ram", 25)
+p1.celebrate_birthday()
+p1.celebrate_birthday()
+
+
 class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
 
-p1 = Person("Raju", 30)
-
-del p1.age
-
-print(p1.name)
-# print(p1.age) #Error
+p1 = Person("Shyam", 36)
+print(p1)
 
 
 class Person:
-    species = "Human"  # Class property
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
+    def __str__(self):
+        return f"{self.name} ({self.age})"
+
+
+p1 = Person("Tilak", 36)
+print(p1)
+
+
+class Playlist:
     def __init__(self, name):
-        self.name = name  # Instance property
+        self.name = name
+        self.songs = []
+
+    def add_song(self, song):
+        self.songs.append(song)
+        print(f"Added: {song}")
+
+    def remove_song(self, song):
+        if song in self.songs:
+            self.songs.remove(song)
+            print(f"Removed: {song}")
+
+    def show_songs(self):
+        print(f"Playlist '{self.name}':")
+        for song in self.songs:
+            print(f"- {song}")
 
 
-p1 = Person("Hatori")
-p2 = Person("Doremee")
+my_playlist = Playlist("Favorites")
+my_playlist.add_song("Bohemian Rhapsody")
+my_playlist.add_song("Stairway to Heaven")
+my_playlist.show_songs()
 
-print(p1.name)
-print(p2.name)
-print(p1.species)
-print(p2.species)
+
+xyz = Playlist("My XYZ")
+xyz.add_song("ABC")
+xyz.add_song("PQR")
+xyz.show_songs()
+xyz.remove_song("ABC")
+xyz.show_songs()
 
 
 class Person:
-    lastname = ""
-
     def __init__(self, name):
         self.name = name
 
+    def greet(self):
+        print("Hello!")
 
-p1 = Person("Ram")
-p2 = Person("Shyam")
 
-Person.lastname = "Pandey"
+p1 = Person("Emil")
 
-print(p1.lastname)
-print(p2.lastname)
+del Person.greet
+
+# p1.greet() #Error
